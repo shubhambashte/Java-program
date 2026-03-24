b580324b80c16a848336b5bc7aa39dfaf9a627bc
shubhambashte <shubhamrbashte@gmail.com>
2026-03-24 18:28:58 +0530

    Variables

---
+++ Variable.java
@@ -0,0 +1,11 @@
+class Variable
+{ 
+    public static void main(String a[])
+    {
+        int num1 = 18;
+        int num2 = 45;
+        int result = num1 + num2;
+        System.out.println(result);
+        
+    }
+}
